1ee1780cbd9f668423c44d2747193fa9df215ce1
Kristjan Fale <kristjan.fale@gmail.com>
2020-01-20 17:05:24 +0100

    GET Request

--- index.js
+++ index.js
@@ -7,11 +7,22 @@ const logger = require('./middleware/logger');
 const app = express();
 
 // Init middleware
-app.use(logger);
+// app.use(logger);
 
 // Get all members
 app.get('/api/members', (req, res) => {
   res.json(members);
+});
+
+// Get single member (req.params.id)
+app.get('/api/members/:id', (req, res) => {
+  const found = members.some(member => member.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json(members.filter(member => member.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
 });
 
 // Set a static folder
